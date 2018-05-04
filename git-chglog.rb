9712f4a31b1c54296d8d29aa07d30e01ce61a140
tsuyoshiwada <wadackel@gmail.com>
2018-05-04 17:00:39 +0900

    0.5.0

--- git-chglog.rb
+++ git-chglog.rb
@@ -1,12 +1,12 @@
 require "formula"
 
 REPOSITORY_URL='https://github.com/git-chglog/git-chglog'
-HOMEBREW_GIT_VERSION='0.4.0'
+HOMEBREW_GIT_VERSION='0.5.0'
 
 class GitChglog < Formula
   homepage "#{REPOSITORY_URL}"
   url "#{REPOSITORY_URL}/releases/download/#{HOMEBREW_GIT_VERSION}/git-chglog_darwin_amd64"
-  sha256 "22ba6c0fad24e2666d53e48f9b7a3d4f99b060481a99beb4f4ac0859dcd060bf"
+  sha256 "617656e140456000a70d6e8db322e711b8da1040dff94df99f23a64273f330c7"
   head "#{REPOSITORY_URL}.git"
   version "#{HOMEBREW_GIT_VERSION}"
 
