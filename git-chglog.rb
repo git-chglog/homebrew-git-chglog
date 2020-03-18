161466e0fbf19673c24afadeafa6ffa778c478ee
Takahiro Ueda <tueda@users.noreply.github.com>
2020-03-18 19:09:07 +0900

    Fix audit failure
    `brew audit --strict` errors are fixed except
      * C: 5: col 3: `url` (line 5) should be put before `version` (line 4)
    which is due to the fact that `url` depends on `version`.
    
    `head` is removed because `brew install --HEAD` did not work correctly.

--- git-chglog.rb
+++ git-chglog.rb
@@ -1,17 +1,16 @@
-require "formula"
-
-REPOSITORY_URL='https://github.com/git-chglog/git-chglog'
-HOMEBREW_GIT_VERSION='0.9.1'
-
 class GitChglog < Formula
-  homepage "#{REPOSITORY_URL}"
-  url "#{REPOSITORY_URL}/releases/download/#{HOMEBREW_GIT_VERSION}/git-chglog_darwin_amd64"
+  desc "CHANGELOG generator implemented in Go"
+  homepage "https://github.com/git-chglog/git-chglog"
+  version "0.9.1"
+  url "#{homepage}/releases/download/#{version}/git-chglog_darwin_amd64"
   sha256 "2023d82b5b505cf8ac4db20f096f439650554e80eb509fe080f5e81b6b477d3c"
-  head "#{REPOSITORY_URL}.git"
-  version "#{HOMEBREW_GIT_VERSION}"
 
   def install
-    system "mv", "git-chglog_darwin_amd64", "git-chglog"
+    mv "git-chglog_darwin_amd64", "git-chglog"
     bin.install "git-chglog"
+  end
+
+  test do
+    system "#{bin}/git-chglog", "--help"
   end
 end
