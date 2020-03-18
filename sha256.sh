c6c7731f53683f1ead43bd67fa761922dcf81848
Takahiro Ueda <tueda@users.noreply.github.com>
2020-03-18 19:36:45 +0900

    Fix the sha256 helper script for the changes in the previous commits

--- sha256.sh
+++ sha256.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 
-REPOSITORY=`grep "REPOSITORY_URL=" git-chglog.rb | sed -E "s/.*'(.+)'$/\1/"`
-VERSION=`grep "GIT_VERSION=" git-chglog.rb | sed -E "s/.*'(.+)'$/\1/"`
+REPOSITORY=`grep "homepage" git-chglog.rb | head -1 | sed -E 's/.*"(.+)"$/\1/'`
+VERSION=`grep "version" git-chglog.rb | head -1 | sed -E 's/.*"(.+)"$/\1/'`
 
-curl -L -O $REPOSITORY/releases/download/$VERSION/git-chglog_darwin_amd64 > /dev/null
+curl -L -O $REPOSITORY/releases/download/$VERSION/git-chglog_darwin_amd64 >/dev/null
+curl -L -O $REPOSITORY/releases/download/$VERSION/git-chglog_linux_amd64 >/dev/null
 openssl dgst -sha256 git-chglog_darwin_amd64
-rm -rf git-chglog_darwin_amd64
+openssl dgst -sha256 git-chglog_linux_amd64
+rm -f git-chglog_darwin_amd64 git-chglog_linux_amd64
